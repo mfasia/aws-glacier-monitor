3a62d93d8f137889106c729e38937eee06e1a383
Tanvir Ahmed <tanvir.ahmed@metafour.com>
2014-07-07 04:21:34 +0000

    Download an archive given a Job ID in chunks

--- src/test/java/net/m4/aws/glacier/VaultsTest.java
+++ src/test/java/net/m4/aws/glacier/VaultsTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -13,7 +14,6 @@ import java.util.Date;
 
 import org.apache.log4j.Logger;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.amazonaws.AmazonClientException;
@@ -23,6 +23,7 @@ import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.event.ProgressEvent;
 import com.amazonaws.event.ProgressListener;
 import com.amazonaws.services.glacier.AmazonGlacierClient;
+import com.amazonaws.services.glacier.TreeHashGenerator;
 import com.amazonaws.services.glacier.model.DescribeJobRequest;
 import com.amazonaws.services.glacier.model.DescribeJobResult;
 import com.amazonaws.services.glacier.model.DescribeVaultOutput;
@@ -61,23 +62,29 @@ public class VaultsTest {
 	public static AmazonGlacierClient glacierClient;
 	public static AmazonSQSClient sqsClient;
     public static AmazonSNSClient snsClient;
+    public static long downloadChunkSize = 4194304; // 4 MB  
 
 	// Jackson JSON Mapper
 	ObjectMapper mapper;
 
 	// metafour - backup
 //	String vaultName = "Backup";
+	String vaultName = "fourdhn2-2014-27";
 //	String jobId = "-X-U6-WqL3HUWbNBtjtif_Jr2L7yWGVwWJ2hAfC4-3JgD_diQ9n92uf_y-bzP01jGrY0ajfw7W-mypRTL42NsoKYEclD";
+//	String jobId = "F3z6zyRc8Gl9lHRePPVgFhzf3y9QpNV7VK9WQm4Gpdu_Cqna3FIGrUw-RgQI9GW4jmX66XhFqtxH_CzfjnbPFD3_cF8K";
+//	String jobId = "dDsW_nDKptnGi-IYjEDMQfk-tKSdKzxNhMqYpeFRW6GP4ZM-8v88k6l9su94MiCkRnNnRbHYvqDcihew2bcV2RXNX4Uy";
+	String jobId = "Ysw6CpgQ0DfewNyMeLHiQJKRLC4EEDPpboSMOgtthjgUCCIOkHpn0HuRoRGqJxNV96I-wE6YgovlWHHS0YVFTCAEZi6G";
 //	String archiveId = "DHPLE7Zkq2fp3bjlaxBOLBKiQzE-7zWLVc9BEz1T1bwzAYDnAdVu_kXVwO8wLrX5T4DvA_IUVSXWJe5hlaPXuMazfGf9k-wkBfy7BHV0L2quqPlAWjm5bpOSoRV6Q-spaw0nKCqttQ";
+	String archiveId = "SlR6vzJqJf4ASIEXpCx6O_Tpn9aUGq_1U7KLdrlJSRBSvYkVYmnNPq-dEsFiBRAWdoK0o3SVxKHAzC33pF13nKA2-5x6MmcIWaFhZsBu725LR0m5iRejJbOKbv9j5JkUuQc9R3ks3w";
 
 	// ftahmed - glacier
-	String vaultName = "testvault";
+//	String vaultName = "testvault";
 //	String jobId = "VmWAzSDpMk6KoR_e5AvdUSrrh42y4ay6pvkgX10UsfTwV7hlz0ShIraU9wRsFuW69nlb7EBEvRIinzJp6BHKg3YXZ9TT"; // Inventory retrieval
 //	String jobId = "12Tb4wniPRDLmCn9DrfrBaDiwY5Wz744EMihqcpTuRPlDgxrFT7_Qxp25VVPhDU7asWf_ZMvUFFLa0TZ9aDch80weV5j"; // Inventory retrieval
 //	String jobId = "UYVO1FsZ8_LnOEEBVa-rM3kQ4s7s6-nfeWsdH9-2DCZzKYc6LAYOrc9nApfNnfh75r1dPpsMO8rLwmtYt0pG56eeI5Bj"; // Archive retrieval
-	String jobId = "6WXMaEyXgkFKvTQUMjOQyOU6LL1cmXG11Fh4jgzwaMOe-x0TrhU4TiZh-tZ7G4c9yyrAu7vmkfFJ1mOhmcOk3Qs7qAsq";
+//	String jobId = "6WXMaEyXgkFKvTQUMjOQyOU6LL1cmXG11Fh4jgzwaMOe-x0TrhU4TiZh-tZ7G4c9yyrAu7vmkfFJ1mOhmcOk3Qs7qAsq";
 //	String jobId = "L7Ecv1hTwnjWZo7850i60hfU7tLrgLB9hFPmA9GNQZT0xwcTmhPhu6L-Q4phB89baEhglUpeZTEkJXksq-76Qb6VTiyr";
-	String archiveId = "gxrGyk6m8ccwFf59SF4gH9OK6w3T638L7NB64camYUtwtWUCtZ0MAyO6JtUnbr6lHaT-kJwSmYMj-DxZS7VRiHPhoQjLAMuRBabldnhtwMv8909W2bK67xmtXiNyQsP4b-UAqBOwvg";
+//	String archiveId = "gxrGyk6m8ccwFf59SF4gH9OK6w3T638L7NB64camYUtwtWUCtZ0MAyO6JtUnbr6lHaT-kJwSmYMj-DxZS7VRiHPhoQjLAMuRBabldnhtwMv8909W2bK67xmtXiNyQsP4b-UAqBOwvg";
 //	String archiveId = "BfUz1-73uq4-FvwEyV1ZrKlPCidvtahjsXwTAu87ThMet-YmNhnOJzLL3KEa-TG43RFwDcpdT1qpuO3P6DcuXK2nxwawkvQUvq_X4ZQdhpSjxIsOQVn7UspjI0xG7smDDfSFneqqWg";
 
 	String archiveToUpload = "target/test-classes/sample.zip";
@@ -174,7 +181,7 @@ public class VaultsTest {
 	 * @throws IOException
 	 */
 	@Test
-	@Ignore
+//	@Ignore
 	public void testInitiateJobInventoryRetrieval() throws IOException {
 		InitiateJobRequest initJobRequest = new InitiateJobRequest()
 				.withAccountId("-").withVaultName(vaultName)
@@ -272,7 +279,7 @@ public class VaultsTest {
 	 * @throws IOException
 	 */
 	@Test
-	@Ignore
+//	@Ignore
 	public void testInitiateJobArchiveRetrieval() throws IOException {
 		InitiateJobRequest initJobRequest = new InitiateJobRequest()
 				.withAccountId("-").withVaultName(vaultName)
@@ -315,7 +322,7 @@ public class VaultsTest {
 
 		FileOutputStream fos = new FileOutputStream(new File(archiveToDownloadJob));
 		BufferedInputStream bis = new BufferedInputStream(joresult.getBody());
-		byte[] buffer = new byte[1024];
+		byte[] buffer = new byte[1024*1024];
 		int c = 0;
 		while ((c = bis.read(buffer)) != -1) {
 			fos.write(buffer, 0, c);
@@ -323,6 +330,73 @@ public class VaultsTest {
 		fos.close();
 		bis.close();
 		logger.info("Job output: " + archiveToDownloadJob);
+
+		assertTrue("Successfully completed.", true);
+	}
+
+	/**
+	 * Download an archive given a Job ID in chunks (assuming the job was initiated 4+ hours ago and completed successfully).
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testJobOutputArchiveRetrievalInChunks() throws IOException {
+		DescribeJobRequest request = new DescribeJobRequest()
+				.withAccountId("-").withVaultName(vaultName).withJobId(jobId);
+		DescribeJobResult result = glacierClient.describeJob(request);
+		logger.info("Job details:");
+		logger.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
+		assertEquals("ArchiveRetrieval", result.getAction());
+		
+		long archiveSizeInBytes = result.getArchiveSizeInBytes();
+		
+		if (archiveSizeInBytes < 0) {
+            System.err.println("Nothing to download.");
+            return;
+        }
+
+        logger.info("archiveSizeInBytes: " + archiveSizeInBytes);
+        FileOutputStream fstream = new FileOutputStream(archiveToDownloadJob);
+        long startRange = 0;
+        long endRange = (downloadChunkSize > archiveSizeInBytes) ? archiveSizeInBytes -1 : downloadChunkSize - 1;
+
+        do {
+
+            GetJobOutputRequest getJobOutputRequest = new GetJobOutputRequest()
+                .withVaultName(vaultName)
+                .withRange("bytes=" + startRange + "-" + endRange)
+                .withJobId(jobId);
+            GetJobOutputResult getJobOutputResult = glacierClient.getJobOutput(getJobOutputRequest);
+
+            BufferedInputStream is = new BufferedInputStream(getJobOutputResult.getBody());     
+            byte[] buffer = new byte[(int)(endRange - startRange + 1)];
+
+            logger.info("Checksum received: " + getJobOutputResult.getChecksum());
+            logger.info("Content range " + getJobOutputResult.getContentRange());
+
+            
+            int totalRead = 0;
+            while (totalRead < buffer.length) {
+                int bytesRemaining = buffer.length - totalRead;
+                int read = is.read(buffer, totalRead, bytesRemaining);
+                if (read > 0) {
+                    totalRead = totalRead + read;                             
+                } else {
+                    break;
+                }
+                
+            }
+            logger.info("Calculated checksum: " + TreeHashGenerator.calculateTreeHash(new ByteArrayInputStream(buffer)));
+            logger.info("read = " + totalRead);
+            fstream.write(buffer);
+            
+            startRange = startRange + (long)totalRead;
+            endRange = ((endRange + downloadChunkSize) >  archiveSizeInBytes) ? archiveSizeInBytes : (endRange + downloadChunkSize); 
+            is.close();
+        } while (endRange <= archiveSizeInBytes  && startRange < archiveSizeInBytes);
+        
+        fstream.close();
+        logger.info("Retrieved file to " + archiveToDownloadJob);
 
 		assertTrue("Successfully completed.", true);
 	}
