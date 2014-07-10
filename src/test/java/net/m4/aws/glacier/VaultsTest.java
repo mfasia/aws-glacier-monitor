8b0d02a39ce1a81ae0e9d166707da1045f358782
Tanvir Ahmed <tanvir.ahmed@metafour.com>
2014-07-10 14:33:13 +0000

    Decode and encode archive description as per mt-aws-glacier (MetaData.pm)

--- src/test/java/net/m4/aws/glacier/VaultsTest.java
+++ src/test/java/net/m4/aws/glacier/VaultsTest.java
@@ -15,9 +15,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Properties;
+import java.util.UUID;
 
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.amazonaws.AmazonClientException;
@@ -51,8 +57,6 @@ import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
-import javax.xml.bind.DatatypeConverter;
-
 /**
  * Tests for AWS Glacier.
  * 
@@ -63,45 +67,34 @@ public class VaultsTest {
 	
 	private static final Logger logger = Logger.getLogger(VaultsTest.class);
 
+	// Test properties keys
+	public static final String VAULT_NAME = "vaultName";
+	public static final String JOB_ID = "jobId";
+	public static final String ARCHIVE_ID = "archiveId";
+	public static final String ARCHIVE_TO_UPLOAD = "archiveToUpload";
+	public static final String ARCHIVE_DOWNLOAD_DIR = "archiveDownloadDir";
+	public static final String ARCHIVE_DESCRIPTION = "archiveDescription";
+	public static final String MT2_ENCODED_ARCHIVE_DESCRIPTION = "mt2EncodedArchiveDescription";
+	// Test properties 
+	private static String vaultName;
+	private static String jobId;
+	private static String archiveId;
+	private static String archiveToUpload;
+	private static String archiveDownloadDir;
+	private static String archiveDescription;
+	private static String mt2EncodedArchiveDescription;
+
 	// AWS
-	public static AWSCredentials credentials;
-	public static AmazonGlacierClient glacierClient;
-	public static AmazonSQSClient sqsClient;
-    public static AmazonSNSClient snsClient;
-    public static long downloadChunkSize = 4194304; // 4 MB  
+	private static AWSCredentials credentials;
+	private static AmazonGlacierClient glacierClient;
+	private static AmazonSQSClient sqsClient;
+	private static AmazonSNSClient snsClient;
+	
+	private static final long DOWNLOAD_CHUNK_SIZE = 4194304; // 4 MB  
 
 	// Jackson JSON Mapper
-	ObjectMapper mapper;
-
-	// metafour - backup
-	String vaultName = "116-demo2.metafour.com";
-//	String vaultName = "304-arrow.m4.net";
-//	String vaultName = "Backup";
-//	String vaultName = "NetCourier";
-//	String vaultName = "fourdhn2-2014-27";
-//	String vaultName = "fourdhn2-2014-28";
-//	String vaultName = "testvault";
+	private static ObjectMapper mapper;
 	
-//	String jobId = "-X-U6-WqL3HUWbNBtjtif_Jr2L7yWGVwWJ2hAfC4-3JgD_diQ9n92uf_y-bzP01jGrY0ajfw7W-mypRTL42NsoKYEclD";
-//	String jobId = "F3z6zyRc8Gl9lHRePPVgFhzf3y9QpNV7VK9WQm4Gpdu_Cqna3FIGrUw-RgQI9GW4jmX66XhFqtxH_CzfjnbPFD3_cF8K";
-//	String jobId = "dDsW_nDKptnGi-IYjEDMQfk-tKSdKzxNhMqYpeFRW6GP4ZM-8v88k6l9su94MiCkRnNnRbHYvqDcihew2bcV2RXNX4Uy";
-	String jobId = "Ysw6CpgQ0DfewNyMeLHiQJKRLC4EEDPpboSMOgtthjgUCCIOkHpn0HuRoRGqJxNV96I-wE6YgovlWHHS0YVFTCAEZi6G";
-//	String archiveId = "DHPLE7Zkq2fp3bjlaxBOLBKiQzE-7zWLVc9BEz1T1bwzAYDnAdVu_kXVwO8wLrX5T4DvA_IUVSXWJe5hlaPXuMazfGf9k-wkBfy7BHV0L2quqPlAWjm5bpOSoRV6Q-spaw0nKCqttQ";
-	String archiveId = "SlR6vzJqJf4ASIEXpCx6O_Tpn9aUGq_1U7KLdrlJSRBSvYkVYmnNPq-dEsFiBRAWdoK0o3SVxKHAzC33pF13nKA2-5x6MmcIWaFhZsBu725LR0m5iRejJbOKbv9j5JkUuQc9R3ks3w";
-
-	// ftahmed - glacier
-//	String vaultName = "testvault";
-//	String jobId = "VmWAzSDpMk6KoR_e5AvdUSrrh42y4ay6pvkgX10UsfTwV7hlz0ShIraU9wRsFuW69nlb7EBEvRIinzJp6BHKg3YXZ9TT"; // Inventory retrieval
-//	String jobId = "12Tb4wniPRDLmCn9DrfrBaDiwY5Wz744EMihqcpTuRPlDgxrFT7_Qxp25VVPhDU7asWf_ZMvUFFLa0TZ9aDch80weV5j"; // Inventory retrieval
-//	String jobId = "UYVO1FsZ8_LnOEEBVa-rM3kQ4s7s6-nfeWsdH9-2DCZzKYc6LAYOrc9nApfNnfh75r1dPpsMO8rLwmtYt0pG56eeI5Bj"; // Archive retrieval
-//	String jobId = "6WXMaEyXgkFKvTQUMjOQyOU6LL1cmXG11Fh4jgzwaMOe-x0TrhU4TiZh-tZ7G4c9yyrAu7vmkfFJ1mOhmcOk3Qs7qAsq";
-//	String jobId = "L7Ecv1hTwnjWZo7850i60hfU7tLrgLB9hFPmA9GNQZT0xwcTmhPhu6L-Q4phB89baEhglUpeZTEkJXksq-76Qb6VTiyr";
-//	String archiveId = "gxrGyk6m8ccwFf59SF4gH9OK6w3T638L7NB64camYUtwtWUCtZ0MAyO6JtUnbr6lHaT-kJwSmYMj-DxZS7VRiHPhoQjLAMuRBabldnhtwMv8909W2bK67xmtXiNyQsP4b-UAqBOwvg";
-//	String archiveId = "BfUz1-73uq4-FvwEyV1ZrKlPCidvtahjsXwTAu87ThMet-YmNhnOJzLL3KEa-TG43RFwDcpdT1qpuO3P6DcuXK2nxwawkvQUvq_X4ZQdhpSjxIsOQVn7UspjI0xG7smDDfSFneqqWg";
-
-	String archiveToUpload = "target/test-classes/sample.zip";
-	String archiveToDownloadJob = "target/JobArchiveRetrieval.bin";
-	String archiveToDownloadAtm = "target/AtmArchiveRetrieval.bin";
 	/**
 	 * Initialises AWS client and Jackson JSON mapper objects.
 	 * 
@@ -109,6 +102,16 @@ public class VaultsTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		Properties p = new Properties();
+		p.load(VaultsTest.class.getResourceAsStream("/VaultsTest.properties"));
+		vaultName = p.getProperty(VAULT_NAME);
+		jobId = p.getProperty(JOB_ID);
+		archiveId = p.getProperty(ARCHIVE_ID);
+		archiveToUpload = p.getProperty(ARCHIVE_TO_UPLOAD);
+		archiveDownloadDir = p.getProperty(ARCHIVE_DOWNLOAD_DIR);
+		archiveDescription = p.getProperty(ARCHIVE_DESCRIPTION);
+		mt2EncodedArchiveDescription = p.getProperty(MT2_ENCODED_ARCHIVE_DESCRIPTION);
+
 		credentials = new PropertiesCredentials(VaultsTest.class.getResourceAsStream("/AwsCredentials.properties"));
 		
 		glacierClient = new AmazonGlacierClient(credentials);
@@ -193,7 +196,7 @@ public class VaultsTest {
 	 * @throws IOException
 	 */
 	@Test
-//	@Ignore
+	@Ignore
 	public void testInitiateJobInventoryRetrieval() throws IOException {
 		InitiateJobRequest initJobRequest = new InitiateJobRequest()
 				.withAccountId("-").withVaultName(vaultName)
@@ -291,7 +294,7 @@ public class VaultsTest {
 	 * @throws IOException
 	 */
 	@Test
-//	@Ignore
+	@Ignore
 	public void testInitiateJobArchiveRetrieval() throws IOException {
 		InitiateJobRequest initJobRequest = new InitiateJobRequest()
 				.withAccountId("-").withVaultName(vaultName)
@@ -332,16 +335,17 @@ public class VaultsTest {
 				.withAccountId("-").withVaultName(vaultName).withJobId(jobId);
 		GetJobOutputResult joresult = glacierClient.getJobOutput(jorequest);
 
-		FileOutputStream fos = new FileOutputStream(new File(archiveToDownloadJob));
+		String fileName = archiveDownloadDir + "/" + UUID.randomUUID().toString();
+		FileOutputStream fos = new FileOutputStream(new File(fileName));
 		BufferedInputStream bis = new BufferedInputStream(joresult.getBody());
-		byte[] buffer = new byte[1024*1024];
+		byte[] buffer = new byte[(int)DOWNLOAD_CHUNK_SIZE];
 		int c = 0;
 		while ((c = bis.read(buffer)) != -1) {
 			fos.write(buffer, 0, c);
 		}
 		fos.close();
 		bis.close();
-		logger.info("Job output: " + archiveToDownloadJob);
+		logger.info("Archive downloaded to: " + fileName);
 
 		assertTrue("Successfully completed.", true);
 	}
@@ -368,9 +372,10 @@ public class VaultsTest {
         }
 
         logger.info("archiveSizeInBytes: " + archiveSizeInBytes);
-        FileOutputStream fstream = new FileOutputStream(archiveToDownloadJob);
+        String fileName = archiveDownloadDir + "/" + UUID.randomUUID().toString();
+        FileOutputStream fstream = new FileOutputStream(fileName);
         long startRange = 0;
-        long endRange = (downloadChunkSize > archiveSizeInBytes) ? archiveSizeInBytes -1 : downloadChunkSize - 1;
+        long endRange = (DOWNLOAD_CHUNK_SIZE > archiveSizeInBytes) ? archiveSizeInBytes -1 : DOWNLOAD_CHUNK_SIZE - 1;
 
         do {
 
@@ -403,12 +408,12 @@ public class VaultsTest {
             fstream.write(buffer);
             
             startRange = startRange + (long)totalRead;
-            endRange = ((endRange + downloadChunkSize) >  archiveSizeInBytes) ? archiveSizeInBytes : (endRange + downloadChunkSize); 
+            endRange = ((endRange + DOWNLOAD_CHUNK_SIZE) >  archiveSizeInBytes) ? archiveSizeInBytes : (endRange + DOWNLOAD_CHUNK_SIZE); 
             is.close();
         } while (endRange <= archiveSizeInBytes  && startRange < archiveSizeInBytes);
         
         fstream.close();
-        logger.info("Retrieved file to " + archiveToDownloadJob);
+        logger.info("Archive downloaded to: " + fileName);
 
 		assertTrue("Successfully completed.", true);
 	}
@@ -425,7 +430,7 @@ public class VaultsTest {
 	public void testUploadArchive() throws AmazonServiceException, AmazonClientException, FileNotFoundException {
 		ArchiveTransferManager atm = new ArchiveTransferManager(glacierClient, sqsClient, snsClient);
         
-        UploadResult result = atm.upload("-", vaultName, "my archive " + (new Date()), new File(archiveToUpload), 
+        UploadResult result = atm.upload("-", vaultName, archiveToUpload + " [" + (new Date()) + "]", new File(archiveToUpload), 
         		new ProgressListener() {
 					public void progressChanged(ProgressEvent e) {
 						logger.info("Bytes transferred: " + e.getBytesTransferred());
@@ -443,13 +448,14 @@ public class VaultsTest {
 		ArchiveTransferManager atm = new ArchiveTransferManager(glacierClient, sqsClient, snsClient);
         
         logger.info("Downloading Archive ID: " + archiveId);
-        atm.download("-", vaultName, archiveId, new File(archiveToDownloadAtm),//);
+        String fileName = archiveDownloadDir + "/" + UUID.randomUUID().toString();
+        atm.download("-", vaultName, archiveId, new File(fileName),
         		new ProgressListener() {
 					public void progressChanged(ProgressEvent e) {
 						logger.info("Bytes transferred: " + e.getBytesTransferred());
 					}
         		});
-        logger.info("Atm output: " + archiveToDownloadAtm);
+        logger.info("Archive downloaded to: " + fileName);
 	}
 
 	/**
@@ -460,8 +466,10 @@ public class VaultsTest {
 	 */
 	@Test
 	public void testDecodeArchiveDescription() {
-		String archiveDescription = "mt2 eyJmaWxlbmFtZSI6InZhci93d3cvbmV0Y3Y0XzExL25jZGVscmVjLmpzIiwibXRpbWUiOiIyMDA4MDgwN1QwNjUwMzFaIn0";
-		String b64 = archiveDescription.split(" ")[1];
+		assertEquals("mt2", mt2EncodedArchiveDescription.split(" ")[0]);
+		String b64 = mt2EncodedArchiveDescription.split(" ")[1];
+        logger.info("Encoded archive description: " + b64);
+		b64 = StringUtils.replaceChars(b64, "-_", "+/");
 		int padding = 4 - (b64.length() % 4);
 		if (padding > 0) {
 			char[] c = new char[padding];
@@ -469,9 +477,31 @@ public class VaultsTest {
 			b64 = b64.concat(new String(c));
 		}
 		byte[] b = DatatypeConverter.parseBase64Binary(b64);
-        logger.info("Decoded archive description: " + new String(b));
+		String decoded = new String(b);
+        logger.info("Decoded archive description: " + decoded);
+		assertEquals(archiveDescription, decoded);
 	}
 
+	/**
+	 * Encode archive description.
+	 * 
+	 * @see
+	 * <a href="https://github.com/vsespb/mt-aws-glacier/blob/master/lib/App/MtAws/MetaData.pm">mt-aws-glacier: MetaData.pm</a>
+	 */
+	@Test
+	public void testEncodeArchiveDescription() {
+		logger.info("Archive description: " + archiveDescription);
+		String b64 = DatatypeConverter.printBase64Binary(archiveDescription.getBytes());
+		logger.info("Encoded archive description: " + b64);
+		// Perl: $res =~ s/=+\z//;
+		String encoded = b64.replaceAll("=+$", ""); // FIXIT
+		// Perl: $res =~ tr{+/}{-_};
+		encoded = StringUtils.replaceChars(b64, "+/", "-_");
+		encoded = "mt2 ".concat(encoded);
+		assertEquals(mt2EncodedArchiveDescription, encoded);
+		logger.info("Encoded archive description: " + encoded);
+	}
+	
 	/**
 	 * Jackson JSON mapper configuration.
 	 * 
